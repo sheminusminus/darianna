389b77fcc2e3698b313d8ca1d7fb8326099d4c2b
Emily Kolar <emkolardev@gmail.com>
2019-01-26 16:02:03 -0600

    separate into master or commands

--- commandUtil.js
+++ commandUtil.js
@@ -6,43 +6,51 @@ const { exec } = require('child_process');
 
 // const envVariables = 'ComSpec=/usr/local/bin/asdfasdf.exe';
 
-async function filesToCommands(masterFilePath, callback) {
-  // read in text file at masterFilePath
-  await fs.readFile(masterFilePath, 'utf8', async (err, fileData) => {
+async function doCommands(commandsFilePath, callback) {
+  // read in the file...
+  fs.readFile(commandsFilePath, 'utf8', async function(err, data) {
     if (err) console.log(err); // handle errors?
 
-    if (fileData) {
-      // split at new lines
-      const files = fileData.split('\n').filter(line => line.length > 0);
+    if (data) {
+      const lines = data.split('\n').filter(line => line.length > 0);
 
-      // for each file of commands listed...
-      await files.forEach(async (filepath) => {
-        // read in the file...
-        fs.readFile(filepath, 'utf8', async function(err, data) {
-          if (err) console.log(err); // handle errors?
+      // for each line in the file...
+      await lines.forEach(async (l, index) => {
+        // treat it as a command
+        // const cmdLine = `${envVariables} ${l}`; // if setting env
 
-          if (data) {
-            const lines = data.split('\n').filter(line => line.length > 0);
+        // EXECUTE HIM
+        await exec(l);
 
-            // for each line in the file...
-            await lines.forEach(async (l, index) => {
-              // treat it as a command
-              // const cmdLine = `${envVariables} ${l}`; // if setting env
-
-              // EXECUTE HIM
-              await exec(l);
-
-              // the last command just finished...
-              if (index === lines.length - 1) {
-                // if we have a callback... call it!
-                if (callback && typeof callback === 'function') callback();
-              }
-            });
-          }
-        });
+        // the last command just finished...
+        if (index === lines.length - 1) {
+          // if we have a callback... call it!
+          if (callback && typeof callback === 'function') callback();
+        }
       });
     }
   });
+}
+
+async function filesToCommands(masterFilePath, commandsFilePath, callback) {
+  if (commandsFilePath) {
+    await doCommands(commandsFilePath, callback);
+  } else {
+    // read in text file at masterFilePath
+    await fs.readFile(masterFilePath, 'utf8', async (err, fileData) => {
+      if (err) console.log(err); // handle errors?
+
+      if (fileData) {
+        // split at new lines
+        const files = fileData.split('\n').filter(line => line.length > 0);
+
+        // for each file of commands listed...
+        await files.forEach(async (filepath) => {
+          await doCommands(filepath);
+        });
+      }
+    });
+  }
 }
 
 module.exports = filesToCommands;
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const filesToCommands = require('./commandUtil');
 
 const filepath = process.argv[2];
+const commandsPath = process.argv[3];
 
 if (filepath) {
-  filesToCommands(filepath, function() {
+  filesToCommands(filepath, commandsPath, function() {
     console.log('done!');
   });
 }
